8088be79517396398d49fdb7b20a26d143444ef4
AnatoliyZhartun <as.zhartun@yandex.by>
2022-01-14 17:23:15 +0300

    Solution is done.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import Calendar.Calendar;
+
+import java.time.Month;
+
 /**
  * Задача 3.
  * Создать класс Календарь с внутренним классом, с помощью объектов которого можно хранить информацию о выходных и праздничных днях.
@@ -5,6 +9,22 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Calendar calendar = new Calendar();
+        calendar.add(
+                new Calendar.Date(13, Month.AUGUST, Calendar.DateType.WEEKDAY),
+                new Calendar.Date(18, Month.FEBRUARY, Calendar.DateType.WEEKEND),
+                new Calendar.Date(3, Month.NOVEMBER, Calendar.DateType.WEEKEND),
+                new Calendar.Date(5, Month.SEPTEMBER, Calendar.DateType.WEEKDAY),
+                new Calendar.Date(1, Month.JANUARY, Calendar.DateType.HOLIDAY)
+        );
+        calendar.print();
+        System.out.println(calendar.getDate(13,Month.AUGUST));
+        System.out.println();
+        System.out.println(calendar.getDateByType(Calendar.DateType.WEEKEND));
+        System.out.println();
+        System.out.println("Remove some dates.");
+        calendar.remove(13, Month.AUGUST);
+        calendar.remove(5, Month.SEPTEMBER);
+        calendar.print();
     }
 }
